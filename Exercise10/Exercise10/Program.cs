b71183954bfd699380d3f1a3c03efcc7de72599a
Yolo518 <145461025+Yolo518@users.noreply.github.com>
2024-03-06 10:05:47 -0500

    m

--- Exercise10/Exercise10/Program.cs
+++ Exercise10/Exercise10/Program.cs
@@ -6,6 +6,7 @@ namespace Exercise10_
     {
         static void Main(string[] args)
         { //Problem 1 Makeing a menu
+            Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("**************");
             Console.WriteLine("Menu:");
             Console.WriteLine("1 - New Game");
@@ -15,8 +16,9 @@ namespace Exercise10_
             Console.WriteLine("**************");
             Console.WriteLine();
             int playerChoice=int.Parse(Console.ReadLine());
-            while(playerChoice>5)
+            while(playerChoice>5 || playerChoice < 1 )
             {
+                Console.WriteLine("Please enter 1-4");
                 Console.WriteLine("**************");
                 Console.WriteLine("Menu:");
                 Console.WriteLine("1 - New Game");
@@ -27,53 +29,21 @@ namespace Exercise10_
                  playerChoice = int.Parse(Console.ReadLine());
                
             }
-            while(playerChoice==1)
+           if(playerChoice==1)
             {
-                Console.WriteLine("Makeing the new world and gods");
-                Console.WriteLine("If it is takeing to long to load prees 1");
-                Console.ReadLine();
-                Console.WriteLine("**************");
-                Console.WriteLine("Menu:");
-                Console.WriteLine("1 - New Game");
-                Console.WriteLine("2 - Load Game");
-                Console.WriteLine("3 - Options");
-                Console.WriteLine("4 - Quit");
-                Console.WriteLine("**************");
-                playerChoice = int.Parse(Console.ReadLine());
+                Console.WriteLine("Make new World");
             }
-            while (playerChoice == 2)
+            if(playerChoice==2)
             {
-                Console.WriteLine("Loading...");
-                Console.WriteLine("There seems to have been a promblem loading the game");
-                Console.ReadLine();
-                Console.WriteLine("**************");
-                Console.WriteLine("Menu:");
-                Console.WriteLine("1 - New Game");
-                Console.WriteLine("2 - Load Game");
-                Console.WriteLine("3 - Options");
-                Console.WriteLine("4 - Quit");
-                Console.WriteLine("**************");
-                playerChoice = int.Parse(Console.ReadLine());
-                
+                Console.WriteLine("loading ...");
             }
-            while (playerChoice == 3)
+            if(playerChoice==3)
             {
                 Console.WriteLine("Volume 100%");
-                Console.WriteLine("Difficulty: Normal");
-                Console.WriteLine("press 2 to exit");
-                Console.ReadLine();
-                Console.WriteLine("**************");
-                Console.WriteLine("Menu:");
-                Console.WriteLine("1 - New Game");
-                Console.WriteLine("2 - Load Game");
-                Console.WriteLine("3 - Options");
-                Console.WriteLine("4 - Quit");
-                Console.WriteLine("**************");
-                playerChoice = int.Parse(Console.ReadLine());
+                Console.WriteLine("Difuclty:Normal");
             }
-            while (playerChoice == 4)
+            if(playerChoice ==4)
             {
-                Console.WriteLine("Quiting");
 
             }
         }
